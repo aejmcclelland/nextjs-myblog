17d58769609d5e6c17ba5dd1bb952e7236434825
aejmcclelland <aejmcclelland@googlemail.com>
2024-03-28 07:26:58 +0000

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import homeStyles from '../styles/home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
 	return (
@@ -8,6 +10,9 @@ export default function Home() {
 			<Head>
 				<title>{siteTitle}</title>
 			</Head>
+			<h3 className={homeStyles.title}>
+				My <Link href='/posts/first-post'>blog posts!</Link>
+			</h3>
 			<section className={utilStyles.headingMd}>
 				<p>Hi,</p>
 				<p>I'm a Graduate Software Developer from Northern Ireland.</p>
